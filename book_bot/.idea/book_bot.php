55c92efffcc1fcb535085acb71bda2f366abcda1
tau0vsegda <tau13613@gmail.com>
2020-07-15 19:11:54 +0300

    15.07

--- book_bot/.idea/book_bot.php
+++ book_bot/.idea/book_bot.php
@@ -1,5 +1,5 @@
 <?php
-  include('vendor/autoload.php'); //Подключаем библиотеку
+  include('vendor/autoload.php'); //
   use Telegram\Bot\Api;
   $telegram = new Api('1031635088:AAFb6oGMm5Ph7SrcO3f4H5wr_mXyOq3sRLo');
   $result = $telegram -> getWebhookUpdates();
@@ -12,29 +12,11 @@
           $reply = "Добро пожаловать в бота!";
           $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
           $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-      }elseif ($text == "/help") {
-          $reply = "Информация с помощью.";
-          $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-      }elseif ($text == "Картинка") {
-          $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-          $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-      }elseif ($text == "Гифка") {
-          $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-          $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-      }elseif ($text == "Последние статьи") {
-          $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-          foreach ($html->channel->item as $item) {
-              $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-          }
-          $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-      }else{
-          $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-          $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
       }
-  }else{
-      $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
-  }
 ?>
 
 
-https://api.telegram.org/bot1031635088:AAFb6oGMm5Ph7SrcO3f4H5wr_mXyOq3sRLo/setWebhook?url=https://track-book-chapters-bot.herokuapp.com//book_bot.php
+//https://api.telegram.org/bot1031635088:AAFb6oGMm5Ph7SrcO3f4H5wr_mXyOq3sRLo/setWebhook?url=https://dashboard.heroku.com/apps/track-book-chapters-bot//book_bot.php
+
+//openssl req -newkey rsa:2048 -sha256 -nodes -keyout https://dashboard.heroku.com/apps/track-book-chapters-bot -x509 -days 365 -out YOURPUBLIC.pem -subj "/C=US/ST=New York/L=Brooklyn/O=Example Brooklyn Company/CN=YOURDOMAIN.EXAMPLE"
+
