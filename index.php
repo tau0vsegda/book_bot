9ae1d55f9998c92801227ff5dcfacf55084d3866
tau0vsegda <61606140+tau0vsegda@users.noreply.github.com>
2020-07-16 00:32:41 +0300

    15.07

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 //  $chat_id = $result["message"]["chat"]["id"];
 //  $name = $result["message"]["from"]["username"];
 //  $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]];
-  if($text){
-      if ($text == "/start") {
-          $reply = "Добро пожаловать в бота!";
-      };
-  };
+    if($text){
+         if ($text == "/start") {
+            $reply = "Добро пожаловать в бота!";
+            $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+        }
+    }
 ?>
