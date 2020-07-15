1d4adca65bc31859a09e5f7cdfc2a4c9a7d3e1c9
tau0vsegda <61606140+tau0vsegda@users.noreply.github.com>
2020-07-16 00:53:05 +0300

    15ю07

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
   $telegram = new Api('1031635088:AAFb6oGMm5Ph7SrcO3f4H5wr_mXyOq3sRLo');
   $result = $telegram -> getWebhookUpdates();
   $text = $result["message"]["text"];
-//  $chat_id = $result["message"]["chat"]["id"];
-//  $name = $result["message"]["from"]["username"];
-//  $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]];
+  $chat_id = $result["message"]["chat"]["id"];
+  $name = $result["message"]["from"]["username"];
+  $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]];
     if($text){
          if ($text == "/start") {
             $reply = "Добро пожаловать в бота!";
