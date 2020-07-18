e67e037847c20c9be08d2971a31f5949a6ea2252
tau0vsegda <tau13613@gmail.com>
2020-07-18 12:31:27 +0300

    16.07

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+header("Content-Type: text/plain");
  function sendMessage($chat_id, $message) 
  {
  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
