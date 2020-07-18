52721b0b940bada2ccfd256ff58b9ff578bb4ff3
tau0vsegda <tau13613@gmail.com>
2020-07-18 15:34:40 +0300

    16.07

--- index.php
+++ index.php
@@ -48,11 +48,11 @@ echo $json;
 $array = json_decode($json,TRUE);
 function test($mas) {
 foreach ($mas as $key => $value) {
-  echo $key . "\n";
+  echo $key . ":\n";
   if (is_array($value)) {
    test($value);
   } else {
-  echo $value;
+  echo $value . "\n";
  }}}
 
  test($array);
