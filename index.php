e571be104ba554e1c522c762b0f3e61edf4fc906
tau0vsegda <tau13613@gmail.com>
2020-07-19 09:56:12 +0300

    16.07

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function sendMessage($chat_id, $message)
  {
  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
  }
- /*
+
  $access_token = '1031635088:AAFb6oGMm5Ph7SrcO3f4H5wr_mXyOq3sRLo';
  $api = 'https://api.telegram.org/bot' . $access_token;
  
@@ -15,72 +15,65 @@ function sendMessage($chat_id, $message)
  $message = $output['message']['text'];
  
  if ($message == '/start') {
-  $preload_text = 'You are welcome, ' . $first_name . '!';
- }
- sendMessage($chat_id, $preload_text);
- //тестовая строка    */
+  sendMessage($chat_id, 'You are welcome, ' . $first_name . '!');
+ }else {
 
-$curl = curl_init();
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=~noragami",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  $curl = curl_init();
 
-curl_close($curl);
+  curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=~$message",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
 
 
 //echo $response;
 
-if (!$err) {
-  $array = new SimpleXMLElement("$response");
-}
-else {echo $err;}
-
-
-function test($mas) {
-foreach ($mas as $key => $value) {
-  if (is_object($value)) {
-   test($value);
+  if (!$err) {
+   $array = new SimpleXMLElement("$response");
   } else {
-  echo $value . "\n";
- }}}
-
-  test($array);
-
-$message = array(
-  "Name" => "",
-  "Summary" => "",
-  "Picture" => "",
-
-);
-/*
-if (!$err) {
-foreach ($array -> manga as $key => $value) {
- foreach ($value as $key1 => $value1) {
-  if (($key1 == "info") && ($value1["type"] == "Main title")) {
-   $message["Name"] = $value1;
+   echo $err;
   }
-  if (($key1 == "info") && ($value1["type"] == "Plot Summary")) {
-   $message["Summary"] = $value1;
-  }
-  if (($key1 == "info") && ($value1["type"] == "Picture")) {
-   $message["Picture"] = $value1["src"];
+
+  $mes = array(
+      "Name" => "",
+      "Summary" => "",
+      "Picture" => "",
+
+  );
+
+  if (!$err) {
+   foreach ($array->manga as $key => $value) {
+    foreach ($value as $key1 => $value1) {
+     if (($key1 == "info") && ($value1["type"] == "Main title")) {
+      $mes["Name"] = $value1;
+     }
+     if (($key1 == "info") && ($value1["type"] == "Plot Summary")) {
+      $mes["Summary"] = $value1;
+     }
+     if (($key1 == "info") && ($value1["type"] == "Picture")) {
+      $mes["Picture"] = $value1["src"];
+     }
+    }
+   }
+   sendMessage($chat_id, $mes["Name"] . "\n" . $mes["Summary"] . "\n" . $mes["Picture"]);
+  } else {
+   sendMessage($chat_id, "we have a problem, sorry...");
   }
  }
-}
-sendMessage($chat_id, $message["Name"] . "\n" . $message["Summary"] . "\n" . $message["Picture"]);
-} else { sendMessage($chat_id, "we have a problem, sorry...");}
-*/
+
 
 
 /*if ($err) {
@@ -92,6 +85,6 @@ sendMessage($chat_id, $message["Name"] . "\n" . $message["Summary"] . "\n" . $me
 //sendMessage($chat_id, "cdn.animenewsnetwork.com/thumbnails/max500x600/encyc/A11608-3.jpg");
 //sendPhoto($chat_id, "cdn.animenewsnetwork.com/thumbnails/max500x600/encyc/A11608-3.jpg");
 
-//sendMessage($chat_id, "я завершил работу");
+sendMessage($chat_id, "я завершил работу");
 
 ?>
