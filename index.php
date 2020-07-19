3b4ddbee80c57489622a74cb1be70dca358653ca
tau0vsegda <tau13613@gmail.com>
2020-07-19 17:37:19 +0300

    16.07

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ function sendMessage($chat_id, $message)
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_ENCODING => "",
       CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
+      CURLOPT_TIMEOUT => 20,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "GET",
   ));
