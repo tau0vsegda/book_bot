9112cc9451c538d0549cec7856a8cd16a4d925fd
tau0vsegda <tau13613@gmail.com>
2020-07-21 13:29:08 +0300

    16.07

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-  function sendMessage($chat_id, $message) {
+/*  function sendMessage($chat_id, $message) {
     file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
   }
 
@@ -104,5 +104,35 @@ If you do not receive a reply for a long time, do not worry, you will receive it
 
     }
   }
-exit;
+exit;*/
+?>
+
+<?php
+$access_token = 'xxx';
+$api = 'https://api.telegram.org/bot' . $access_token;
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$message = $output['message']['text'];
+$callback_query = $output['callback_query'];
+$data = $callback_query['data'];
+$message_id = ['callback_query']['message']['message_id'];
+$chat_id_in = $callback_query['message']['chat']['id'];
+switch($message) {
+  case '/test':
+    $inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
+    $inline_button2 = array("text"=>"work plz","callback_data"=>'/plz');
+    $inline_keyboard = [[$inline_button1,$inline_button2]];
+    $keyboard=array("inline_keyboard"=>$inline_keyboard);
+    $replyMarkup = json_encode($keyboard);
+    sendMessage($chat_id, "ok", $replyMarkup);
+    break;
+}
+switch($data){
+  case '/plz':
+    sendMessage($chat_id_in, "plz");
+    break;
+}
+function sendMessage($chat_id, $message, $replyMarkup) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&reply_markup=' . $replyMarkup);
+}
 ?>
