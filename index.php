9d2c85ceb3211a851b4eccae91268544309a7083
tau0vsegda <tau13613@gmail.com>
2020-07-21 21:19:26 +0300

    16.07

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ If you do not receive a reply for a long time, do not worry, you will receive it
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=" . "15584",//. $manga,
+      CURLOPT_URL => "https://cdn.animenewsnetwork.com/encyclopedia/api.xml?manga=" . $manga,
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_TIMEOUT => 30,
@@ -52,6 +52,8 @@ If you do not receive a reply for a long time, do not worry, you will receive it
 
     if (!$err) {
       $array = new SimpleXMLElement($response);
+
+      if ($array == "") {sendMessage($chat_id_in, "пустой массив");}
 
       foreach ($array as $key => $value) {
 
