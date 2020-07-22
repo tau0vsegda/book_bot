c8ec76d0b1db4b5ace1f4c6c33a24e8619012dda
tau0vsegda <tau13613@gmail.com>
2020-07-22 18:00:07 +0300

    17.07

--- index.php
+++ index.php
@@ -41,8 +41,8 @@ if ($message == "/start")
     {
         foreach ($value as $value1)
         {
-            sendMessage($chat_id, "" . $value1);
-            if ((string) $value === (string) $chat_id)
+
+            if ((string) $value == (string) $chat_id)
             {
                 $consist = true;
                 sendMessage($chat_id, "ты уже есть в базе данных");
