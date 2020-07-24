de83e949f305c77d9d60213c71733f73353c17a6
tau0vsegda <tau13613@gmail.com>
2020-07-24 18:31:17 +0300

    17.07

--- workWithDB.php
+++ workWithDB.php
@@ -1,5 +1,4 @@
 <?php
-require('messageHandling.php');
 const DB_DSN = "mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_8869f6e7e3f5fac";
 const DB_USER = "b01c10efa93d3a";
 const DB_PASSWORD = "363d0b63";
