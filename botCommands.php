fc268f558b1c7af528c0672b3a8788457be7fe67
tau0vsegda <tau13613@gmail.com>
2020-07-24 19:31:43 +0300

    17.07

--- botCommands.php
+++ botCommands.php
@@ -59,7 +59,7 @@ function foundMangaMessage($chatID, $message)
                     if (WordExist($words, $mangaParameters["name"]))
                     {
                         $databasesUsers = selectFromTable("id", "users", "chat_id = '{$chatID}'");
-                        $userID = [0]["id"];
+                        $userID = $databasesUsers[0]["id"];
                         insertIntoTable("temp_manga_data", "user_id = '{$userID}', manga_name = '{$mangaParameters["name"]}', manga_id = '{$mangaParameters["id"]}'");
                         $information = searchParameters($mangaParameters);
                         if ($information !== null)
